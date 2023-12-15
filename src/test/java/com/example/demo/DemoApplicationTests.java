5cd487eaf3349d674dfc252c683ebc2ae422f18e
Nils Breunese <nbreunese@bol.com>
2023-12-15 12:03:21 +0100

    Add project files

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.observation.ServerRequestObservationConvention;
+
+import static org.assertj.core.api.Assertions.*;
+
+class DemoApplicationTests {
+
+	@Test
+	void fails_on_startup_when_no_unique_ServerRequestObservationConvention_is_present() {
+		assertThatException()
+				.isThrownBy(() -> SpringApplication.run(ApplicationWithTwoCustomServerRequestObservationConventionBeans.class))
+				.withRootCauseInstanceOf(NoUniqueBeanDefinitionException.class);
+	}
+
+	@SpringBootApplication
+	static class ApplicationWithTwoCustomServerRequestObservationConventionBeans {
+
+		@Configuration
+		static class Config {
+
+			@Bean
+			ServerRequestObservationConvention serverRequestObservationConvention1() {
+				return new ServerRequestObservationConvention() {
+					@Override
+					public String getName() {
+						return "Custom convention 1";
+					}
+				};
+			}
+
+			@Bean
+			ServerRequestObservationConvention serverRequestObservationConvention2() {
+				return new ServerRequestObservationConvention() {
+					@Override
+					public String getName() {
+						return "Custom convention 2";
+					}
+				};
+			}
+		}
+	}
+}
